992130d9935bb16e8cbfc2dbb90e4ee21d949536
«Vitaliy» <955550@list.ru>
2022-08-07 21:59:38 +0300

    ToLowerIfElse

---
+++ Testing005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "сергей")
+{
+    Console.WriteLine("Привет, ты же АЛКОГОЛИК! ");
+}
+else
+{
+    Console.Write("Привет, нормальный человек ");
+    Console.WriteLine(username);
+}
